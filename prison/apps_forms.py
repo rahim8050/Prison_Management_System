106ca9f4fcfa940cc29c8140fb8718d3a1f3fae9
rahim8050 <58936541+rahim8050@users.noreply.github.com>
2025-05-13 08:59:47 +0100

    Update apps_forms.py

--- prison/apps_forms.py
+++ prison/apps_forms.py
@@ -27,4 +27,4 @@ class ArmouryForm(forms.ModelForm):
             'GunModel': forms.TextInput(attrs={'class': 'form-control'}),
         }
 
-    password = forms.CharField(widget=forms.PasswordInput)
+    
